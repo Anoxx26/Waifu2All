b713c1fa09bf3496c7311e4765b3ceca5bea81fa
Alexey <petrovalexei2004103@mail.ru>
2024-08-12 00:00:02 +0500

    Waifu2All v2.1

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import HedaerNavButton from './header-nav-button';
 
 const StyledHeader = styled.header`
-    height: 60px;
+    height: 7vh;
     margin-left: 5rem;
     margin-right: 5rem;
     margin-top: 0px;
@@ -10,7 +10,16 @@ const StyledHeader = styled.header`
     border-bottom-left-radius: 25px;
     border-bottom-right-radius: 25px;
     background-color: #2A2A30;
-    min-width: 200px
+    min-width: 200px;
+
+
+    @media (max-width: 768px) {
+        height: 7vh;
+        margin-left: 1rem;
+        margin-right: 1rem;
+        font-size: 1.2rem;
+    }
+
 `;
 
 const StyledNav = styled.nav`
@@ -20,17 +29,24 @@ const StyledNav = styled.nav`
     flex-direction: row;
     align-items: center;
     justify-content: space-around;
+
 `;
 
 const Logo = styled.h1`
     @import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&display=swap');
 
     font-weight: 600;
-    font-size: 32px;
+    font-size: 34px;
+    margin: 0px;
     letter-spacing: 2px;
     color: #fff;
     font-family: 'Karla', sans-serif;
-    user-select: None
+    user-select: None;
+
+    @media (max-width: 768px) {
+        font-size: 24px;
+    }
+
 `;
 
 export default function Header(){
